bb4b88d74d8fc544bc4946dbb5d8adb036cac66c
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-02-10 03:13:07 -0500

    boilerplate

--- src/main/java/tfar/ps1packtweaks/mixin/ExampleMixin.java
+++ src/main/java/tfar/ps1packtweaks/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.examplemod.mixin;
+package tfar.ps1packtweaks.mixin;
 
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +10,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
 	}
 }
